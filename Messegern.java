c9d14a337a10d46d46db69eb074d1b366f6bfe35
stevezhuravel <stevezhuravel@gmail.com>
2019-01-05 12:28:39 -0500

    fixing Messegern class

---
+++ Messegern.java
@@ -0,0 +1,17 @@
+public class Messegern{
+
+  private String receive;
+  private String sent;
+}
+public Messegern(String receive, String sent){
+  this.receive=receive;
+  this.sent=sent;
+}
+
+public String setMessegern(String sent){
+ return this.sent;
+}
+public String getMessegern(String receive){
+  return this.receive;
+}
+}
---
+++ Status.java
@@ -0,0 +1,13 @@
+public class Status{
+  private String Freshman;
+  private String Sophomore;
+  private String Junior;
+  private String Senior;
+
+}
+public Status(String Freshman, String Sophomore, String Junior, String Senior){
+  this.Freshman=Freshman;
+  this.Sophomore=Sophomore;
+  this.Junior=Junior;
+  this.Senior=Senior;
+}
