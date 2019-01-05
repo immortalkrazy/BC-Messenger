c05b0e41c180908f7abc6e7b5313b8d71d62fbfd
stevezhuravel <stevezhuravel@gmail.com>
2019-01-05 12:33:50 -0500

    fix Messegern setters and getters

--- Status.java
+++ Status.java
@@ -4,10 +4,11 @@ public class Status{
   private String Junior;
   private String Senior;
 
-}
+
 public Status(String Freshman, String Sophomore, String Junior, String Senior){
   this.Freshman=Freshman;
   this.Sophomore=Sophomore;
   this.Junior=Junior;
   this.Senior=Senior;
+}
 }
